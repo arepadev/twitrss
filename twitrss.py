5fc3599e0d5ca8a51d9da3f13c972a21369a3e94
Wil Alvarez <wil.alejandro@gmail.com>
2012-01-31 16:02:12 -0300

    Implemented methods to list/add/del feeds

--- twitrss.py
+++ twitrss.py
@@ -9,24 +9,40 @@ configured accounts """
 # Organization: ArepaDev <http://github.com/arepadev>
 # Jan 26, 2012
 
+import sys
 import time
+import Queue
 import logging
 import sqlite3
 import feedparser
 
 from optparse import OptionParser
 
-POLLING_TIME = 5 #min
-RSS_FEED = 'http://damncorner.blogspot.com/feeds/posts/default'
-FIND_FEED = 'SELECT * FROM Feeds WHERE link = ?'
-INSERT_NEW_CONTROL = 'INSERT INTO Control (id,last_update) VALUES (?,?)'
-INSERT_NEW_FEED = 'INSERT INTO Feeds (title,link,created,updated) VALUES (?,?,?,?)'
+POSTING_TIME = 1 # min
+POLLING_TIME = 5 # min
+#RSS_FEED = 'http://damncorner.blogspot.com/feeds/posts/default'
+
+# Queries
+SELECT_ALL_FEEDS = 'SELECT * FROM Feeds'
+SELECT_FEED = 'SELECT * FROM Feeds WHERE url = ?'
+INSERT_FEED = 'INSERT INTO Feeds (url) VALUES (?)'
+DELETE_FEED = 'DELETE FROM Feeds WHERE id = ?'
+
+SELECT_POST = 'SELECT * FROM Posted WHERE link = ?'
+INSERT_CONTROL = 'INSERT INTO Control (id,last_update) VALUES (?,?)'
+#INSERT_FEED = 'INSERT INTO Feeds (title,link,created,updated) VALUES (?,?,?,?)'
 
 class TwitRss:
     def __init__(self):
         parser = OptionParser()
         parser.add_option('-d', '--debug', dest='debug', action='store_true',
             help='show debug info in shell during execution', default=False)
+        parser.add_option('-a', '--add-feed', dest='feed_to_add',
+            help='add a feed to database')
+        parser.add_option('-l', '--list-feeds', dest='list_feeds', 
+            action='store_true', help='list all feeds', default=False)
+        parser.add_option('-r', '--remove-feed', dest='feed_to_remove',
+            help='remove a feed to database')
         
         (options, args) = parser.parse_args()
         
@@ -36,11 +52,24 @@ class TwitRss:
             logging.basicConfig(level=logging.INFO)
         
         self.log = logging.getLogger('TwitRSS')
-        self.log.debug("Starting")
-        
         self.log.debug("Setting up database connection")
         self.connection = sqlite3.connect('database.db')
         self.cursor = self.connection.cursor()
+        
+        if options.feed_to_add:
+            self.add_feed(options.feed_to_add)
+            self.quit()
+            
+        if options.feed_to_remove:
+            self.remove_feed(options.feed_to_remove)
+            self.quit()
+        
+        if options.list_feeds:
+            self.list_feeds()
+            self.quit()
+        
+        self.log.info("Starting service")
+        self.queue = Queue.Queue()
     
     def __get_last_update(self):
         self.cursor.execute('SELECT * FROM Control WHERE id = 1')
@@ -61,6 +90,37 @@ class TwitRss:
         self.cursor.execute('UPDATE Control SET last_update = ? WHERE id = ?', update)
         self.connection.commit()
         self.last_update = update[0]
+    
+    def add_feed(self, url):
+        self.log.debug('Adding feed %s...' % url)
+        self.cursor.execute(SELECT_FEED, (url, ))
+        if self.cursor.fetchone():
+            self.log.info('Feed already exist in database')
+        else:
+            self.cursor.execute(INSERT_FEED, (url, ))
+            self.connection.commit()
+            self.log.info('Feed added successfully')
+    
+    def list_feeds(self):
+        self.log.debug('Listing feeds')
+        self.cursor.execute(SELECT_ALL_FEEDS)
+        feeds = self.cursor.fetchall()
+        if len(feeds) > 0:
+            print '  ID   URL'
+            print '=' * 80
+            for feed in feeds:
+                print "%4s   %s" % (feed[0], feed[1])
+        else:
+            self.log.info('There are no feeds registered')
+    
+    def remove_feed(self, id_):
+        self.log.debug('Removing feed with id %s' % id_)
+        self.cursor.execute(DELETE_FEED, (id_,))
+        self.connection.commit()
+        if self.cursor.rowcount > 0:
+            self.log.info('Feed removed successfully')
+        else:
+            self.log.info('That feed was not found in database')
         
     def main(self):
         while True:
@@ -90,8 +150,15 @@ class TwitRss:
                 time.sleep(POLLING_TIME * 60)
             except KeyboardInterrupt:
                 break
+            
+        self.quit()
+    
+    def quit(self, error=False):
         self.connection.close()
         self.log.debug('Bye')
+        if error:
+            sys.exit(-1)
+        sys.exit(0)
         
 
 if __name__ == "__main__":
